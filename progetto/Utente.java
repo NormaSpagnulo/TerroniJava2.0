bd0f1d9136e92163be83b68159c0b0848faf455b
Norma Spagnulo <125296943+NormaSpagnulo@users.noreply.github.com>
2023-03-20 17:38:12 +0100

    verificato e commentato

--- progetto/Utente.java
+++ progetto/Utente.java
@@ -1,26 +1,33 @@
 import java.util.ArrayList;
-public class Utente{
+
+public class Utente {
+    //dichiarate variabili
     private String username;
     private String password;
-    private boolean admin=false;
-    public Utente(String username,String password,String passAdmin){
-        if(passAdmin.equals("ciaoAdmin")){
-            admin=true;
-        }else{
-            admin=false;
+    private boolean admin = false;
+    
+    //creato il costruttore
+    public Utente(String username, String password, String passAdmin) {
+        if (passAdmin.equals("ciaoAdmin")) {
+            admin = true;
+        } else {
+            admin = false;
         }
-        this.username=username;
-        this.password=password;
+        this.username = username;
+        this.password = password;
     }
-    public Utente(String username,Boolean admin){
-        this.admin=admin;
-        this.username=username;
-        
+
+    public Utente(String username, Boolean admin) {
+        this.admin = admin;
+        this.username = username;
+
     }
-    public boolean getAdmin(){
+
+    public boolean getAdmin() {
         return admin;
     }
-    public String getUsername(){
+
+    public String getUsername() {
         return username;
     }
 }
