73d1c6d1075b13348384369e9f5bc8698702914f
Norma Spagnulo <125296943+NormaSpagnulo@users.noreply.github.com>
2023-03-20 17:37:30 +0100

    verificato codice e commentato

--- progetto/mainProve.java
+++ progetto/mainProve.java
@@ -11,9 +11,12 @@ public class mainProve {
         int temp;
         gestioneDb myGestiore = new gestioneDb(myConnessione.getConnessione());
         do {
-            if (loggato == null) {
+            //richiesta all'utente di iscriversi o di loggarsi
+            if (loggato == null) {   
                 switch (scanInt("Inserisci \n1:Iscriverti \n2:login")) {
+                    //nel primo caso, l'utente si iscriverà
                     case 1:
+                        //verrà richiesto se è un admin o no
                         if (scanInt("Vuoi iscriverti come admin? 1:si 0:no") == 1) {
                             username = scanString("Inserisci username");
                             myGestiore.aggiungiUtente(username, scanString("Inserisici password"),
@@ -22,35 +25,40 @@ public class mainProve {
                             username = scanString("Inserisci username");
                             myGestiore.aggiungiUtente(username, scanString("Inserisici password"), " ");
                         }
+                        //verrà richiesto di inserire gli animali inserendo nome, razza e anno di nascita
                         if (scanInt("Vuoi inserire animali? 1:si 0:no") == 1) {
                             myGestiore.aggiungiAnimale(scanString("Inserisci nome animale"),
                                     scanString("Inserisci razza"), scanInt("Inserisci anno di nascita"), username);
                         }
                         break;
-
+                    
+                    //nel secondo caso, l'utente effettuerà il login inserendo nome utente e password
                     case 2:
                         loggato = myGestiore.login(scanString("Inserisci nome utente"),
                                 scanString("Inserisci password"));
 
                         break;
+                    // nel terzo caso, ovvero nel caso di terminazione uscirà dal ciclo e tornerà al menù iniziale
                     case 3:
                         uscita = true;
                         break;
+
+                    //nel caso di default ci sarà un avviso di inserimento errato
                     default:
                         System.out.println("Inserimento errato");
                 }
             } else if (loggato != null) {
                 System.out.println(loggato.getAdmin());
                 if (loggato.getAdmin() == true) {
-                    loggato=menu.menuAdmin(myGestiore, loggato);
+                    loggato = menu.menuAdmin(myGestiore, loggato);
                 } else {
-                    loggato=menu.menuUtente(myGestiore, loggato);
+                    loggato = menu.menuUtente(myGestiore, loggato);
                 }
             }
 
         } while (uscita != true);
     }
-
+    //metodo per prendere in input dall'utente un intero
     static int scanInt(String testo) {
         try {
             Scanner myScan = new Scanner(System.in);
@@ -62,7 +70,7 @@ public class mainProve {
         }
 
     }
-
+    //metodo per prendere in input dall'utente una stringa
     static String scanString(String testo) {
         try {
             Scanner myScan = new Scanner(System.in);
